a40dd00922e405e81f48728db9553d120e1f0384
Rike-Benjamin Schuppner <debilski@debilski.de>
2026-09-01 13:57:31 +0200

    halt

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-mono dark:bg-black">
       <main className="flex flex-1 w-full max-w-4xl flex-col items-center justify-between py-16 px-16 bg-white dark:bg-black sm:items-start">
-        <EditableSimulator defaultSource={""}></EditableSimulator>
+        <EditableSimulator defaultSource={"halt"}></EditableSimulator>
       </main>
     </div>
   );
